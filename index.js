3779cc1ef40d42033ffc726dadcac2185b4c1843
Danial Hasan <danialhasan14@gmail.com>
2022-03-14 22:54:55 -0400

    configured command deploy script

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-require('dotenv').config()
-const { Client, Intents } = require('discord.js');
+require("dotenv").config();
+const axios = require("axios");
+const { Client, Intents } = require("discord.js");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.once('ready', () => {
-	console.log('Ready!');
+client.on("interactionCreate", async (interaction) => {
+    console.log("Interaction logged: ", interaction);
+    const response = await axios.get("https://aws.random.cat/meow");
+});
+
+client.once("ready", () => {
+    console.log("Ready!");
 });
 
 // Login to Discord with your client's token
